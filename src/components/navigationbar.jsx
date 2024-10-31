6715f61dc921fedfcf33b6589943216cbc520a2a
CharlesKristov <charlesdarmawan1@gmail.com>
2024-11-01 00:00:34 +0700

    fix navbar

--- src/components/navigationbar.jsx
+++ src/components/navigationbar.jsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useSession, signOut } from "next-auth/react"; // Import useSession and signOut
 
 import logo from "../assets/logo.png";
+import logosmall from "../assets/logosmall.png";
+
 
 export default function NavigationBar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -15,55 +17,60 @@ export default function NavigationBar() {
 
   return (
     <>
-      <nav className="bg-white-800 border-b h-20">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <nav className="bg-white-800 border-b h-20 flex items-center justify-center">
+        <div className="size-full md:size-3/4">
           <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-              {/* Mobile menu button */}
-              <button
-                type="button"
-                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded={isMobileMenuOpen}
-                onClick={handleMobileMenuToggle}
-              >
-                <span className="absolute -inset-0.5"></span>
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="block h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
+            <div className="flex flex-1 items-center justify-between m-3 md:items-end md:justify-between md:align-center md:p-3">
+              <div className=" inset-y-0 left-0 flex items-center md:hidden">
+                {/* Mobile menu button */}
+                <button
+                  type="button"
+                  className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded={isMobileMenuOpen}
+                  onClick={handleMobileMenuToggle}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-                <svg
-                  className="hidden h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
+                  <span className="absolute -inset-0.5"></span>
+                  <span className="sr-only">Open main menu</span>
+                  <svg
+                    className="block h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
+                  <svg
+                    className="hidden h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
 
-            <div className="flex flex-1 items-center justify-center md:items-end md:justify-center md:align-center">
-              <div className="flex flex-shrink-0 items-center mt-2">
+              <div className="hidden md:flex md:flex-shrink-0 md:items-center md:mt-2">
                 <Link href={"/"}>
                   <Image alt="DGU Logo" src={logo} height={"50"} />
                 </Link>
               </div>
+              <div className="flex flex-shrink-0 items-center mt-2 md:hidden">
+                <Link href={"/"}>
+                  <Image alt="DGU Logo" src={logosmall} height={"50"} />
+                </Link>
+              </div>
 
-              <div className="hidden md:ml-6 md:flex md:align-center md:items-center">
+              <div className="hidden md: md:flex md:align-center md:items-center">
                 <div className="flex space-x-4 justify-center text-center">
                   <Link
                     className="relative px-1 text-sm font-medium text-gray-800 no-underline before:absolute before:bottom-0 before:left-0 before:w-full before:h-[2px] before:bg-black before:scale-x-0 before:origin-left hover:before:scale-x-100 before:transition before:duration-300"
@@ -103,11 +110,7 @@ export default function NavigationBar() {
                   </Link>
                 </div>
 
-                <div className="flex space-x-4 justify-center text-center pl-3">
-                  <Link href={"/request-form"} className="relative rounded-md bg-gray-700 px-2 py-2 text-white text-sm font-small text-gray-600 no-underline before:absolute before:bottom-0 before:left-0 before:w-full before:bg-black before:scale-x-0 before:origin-left before:transition before:duration-300 hover:bg-gray-600 active:bg-gray-800">
-                    Request Form
-                  </Link>
-                </div>
+                
 
                 {/* User details if logged in */}
                 {session && session.user && session.user.role === "admin" && (
@@ -132,16 +135,21 @@ export default function NavigationBar() {
                     </button>
                   </div>
                 )}
-
-                
               </div>
+              
+              <div className="md:flex md:space-x-4 md:justify-center md:text-center">
+                <Link href={"/request-form"} className="relative rounded-md bg-gray-700 px-2 py-2 text-white text-sm font-small text-gray-600 no-underline before:absolute before:bottom-0 before:left-0 before:w-full before:bg-black before:scale-x-0 before:origin-left before:transition before:duration-300 hover:bg-gray-600 active:bg-gray-800">
+                  Request Form
+                </Link>
+              </div>
+
             </div>
           </div>
         </div>
 
         {/* Mobile menu, show/hide based on menu state */}
         {isMobileMenuOpen && (
-          <div className="absolute w-100 z-50 md:hidden" id="mobile-menu">
+          <div className="absolute w-100 z-50 md:hidden top-20" id="mobile-menu">
             <div className="space-y-1 px-2 pb-3 pt-2 bg-gray-800">
               <Link
                 href="/#products"
